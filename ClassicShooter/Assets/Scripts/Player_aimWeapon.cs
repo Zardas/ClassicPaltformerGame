2f73fd29b2b48a96599c634b74866e31da354657
Antoine <antoineg.work@gmail.com>
2020-02-05 02:44:51 +0100

    Gestion mouvement et ce maudit probleme de rotation de gun enfin resolu

---
+++ ClassicShooter/Assets/Scripts/Player_aimWeapon.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_aimWeapon : MonoBehaviour
+{
+    //private Transform aimTransform;
+
+    private Animator animator;
+
+    [SerializeField]
+    GameObject aim;
+
+    [SerializeField]
+    GameObject gun;
+
+    private bool alreadyRotated;
+
+    private void Awake()
+    {
+        this.alreadyRotated = false;
+        //this.aimTransform = transform.Find("Aim");
+        //this.animator = gun.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gunRotation();
+        this.gunShooting();
+    }
+
+
+
+    private void gunRotation()
+    {
+        Vector3 mousePosition = GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aim.transform.eulerAngles = new Vector3(0, 0, angle);
+
+        if(aim.transform.eulerAngles.z > 90 && aim.transform.eulerAngles.z < 280 && !alreadyRotated)
+        {
+            gun.transform.Rotate(180f, 0, 0);
+            alreadyRotated = !alreadyRotated;
+        }
+        if(!(aim.transform.eulerAngles.z > 90 && aim.transform.eulerAngles.z < 280) && alreadyRotated)
+        {
+            gun.transform.Rotate(180f, 0, 0);
+            alreadyRotated = !alreadyRotated;
+        }
+
+        Debug.Log("Rotation actuelle : " + aim.transform.eulerAngles);
+        /*if (Input.GetKey("q") || Input.GetKey("right"))
+        {
+            aimTransform.Translate(0, 1, 0);
+        }*/
+    }
+
+
+    private void gunShooting()
+    {
+        if (Input.GetMouseButtonDown(0)) // Click gauche
+        {
+            //animator.SetTrigger("Shoot");
+        }
+    }
+
+
+    // From codeMonkey : https://unitycodemonkey.com/video.php?v=fuGQFdhSPg4
+    // Get Mouse Position in World with Z = 0f
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+    public static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
---
+++ ClassicShooter/Assets/Scripts/Player_movement.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+
+    Rigidbody2D rigidBody2D;
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    Transform groundCheck;
+
+    [SerializeField]
+    Transform groundCheckLeft;
+
+    [SerializeField]
+    Transform groundCheckRight;
+
+    [SerializeField]
+    float jumpSpeed = 6;
+
+    [SerializeField]
+    float runSpeed = 6;
+
+    [SerializeField]
+    GameObject gun;
+
+    bool isGrounded;
+    bool faceRight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidBody2D = GetComponent<Rigidbody2D>();
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+
+        this.faceRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        //Check if the player is on a floor (and so if he can jump)
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")) ||
+           Physics2D.Linecast(transform.position, groundCheckLeft.position, 1 << LayerMask.NameToLayer("Ground")) ||
+           Physics2D.Linecast(transform.position, groundCheckRight.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+
+
+        if (Input.GetKey(KeyCode.LeftArrow)) // LEFT
+        {
+
+            rigidBody2D.velocity = new Vector2(-runSpeed, rigidBody2D.velocity.y);
+
+            if (this.faceRight)
+            {
+                this.flip();
+            }
+
+            if (this.isGrounded)
+            {
+                //animator.Play("Player_run");
+            }
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) // DROITE
+        {
+            rigidBody2D.velocity = new Vector2(runSpeed, rigidBody2D.velocity.y);
+
+            if (!this.faceRight)
+            {
+                this.flip();
+            }
+
+            if (this.isGrounded)
+            {
+                //animator.Play("Player_run");
+            }
+        }
+        else
+        {
+            rigidBody2D.velocity = new Vector2(0, rigidBody2D.velocity.y);
+
+            if (isGrounded)
+            {
+                //animator.Play("Player_idle");
+            }
+        }
+
+
+
+        // SAUT
+        if (Input.GetKey(KeyCode.UpArrow) && isGrounded)
+        {
+            rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpSpeed);
+            //animator.Play("Player_jump");
+        }
+    }
+
+
+    private void flip()
+    {
+        this.faceRight = !this.faceRight;
+        //spriteRenderer.flipX = !this.faceRight;
+        transform.Rotate(0, 180f, 0);
+
+        //gun.transform.Rotate(0, 0, 180f);
+    }
+}
